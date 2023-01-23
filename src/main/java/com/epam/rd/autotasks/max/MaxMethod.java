5f89c13d2ba1c2ed085ec87da755b715ddebdec4
Sadique Iqbal <sadiqueiqbal.si@gmail.com>
2023-01-23 15:23:08 +0530

    Jugaad

--- src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
+++ src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
@@ -1,7 +1,30 @@
 package com.epam.rd.autotasks.max;
+import java.util.*;
 
 public class MaxMethod {
     public static int max(int[] values) {
-        throw new UnsupportedOperationException();
+      /*  Scanner s=new Scanner(System.in);
+
+        for(int i=0;i<values.length;i++){
+            if(values.length==1){
+                return values[i];
+            }
+            int m=values[0];
+            if (values[i]<Integer.MIN_VALUE ){
+                return Integer.MIN_VALUE;
+            } else if (values[i]>Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
+            } else if(m<=values[i]){
+                m=values[i];
+                return m;
+            }
+
+        }*/
+         int max_value=0;//jugaad
+        Arrays.sort(values);
+        max_value=values[values.length-1];
+        return  max_value;
+      // throw new UnsupportedOperationException();
+
     }
 }
